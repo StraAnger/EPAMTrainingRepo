967981af66e0329aaadfa5143f876f20b86e86cc
StraAnger <Stranger.01@mail.ru>
2022-01-23 04:24:31 +0400

    Task 2.2 pending check

---
+++ Task 2/Task 2.2/GameField.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_2_1;
+
+public class GameField
+{
+    // Properties
+
+    public int Height { get; set; }
+    public int Width { get; set; }
+
+    public char[,] GameFieldMatrix { get; set; }
+
+    // Constructors
+
+    public GameField(int height, int width)
+    {
+        this.Height = height;
+        this.Width = width;
+
+        GameFieldMatrix = new char[Height, Width];
+
+    }
+
+    public GameField(char[,] inputGameFieldMatrix)
+    {
+        GameFieldMatrix = inputGameFieldMatrix;
+    }
+
+}
+
+
+
+
---
+++ Task 2/Task 2.2/GameFieldObjects.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_2_1;
+
+public class GameFieldObjects
+{
+    public int CoordinateX { get; set; }
+    public int CoordinateY { get; set; }
+
+}
+
+
+
+
